87b1f50275c1b06a181afc341bd38ef2cfe2574e
hysabelly <90413604+hysabelly@users.noreply.github.com>
2022-10-17 23:17:07 -0300

    Add files via upload

---
+++ IntroEntityFramework-main/IntroEntityFramework-main/IntroEntityFramework/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IntroEntityFramework.Models;
+
+Console.WriteLine("Hello, World!");
+
+SystemContext context = new SystemContext();
+context.Database.EnsureCreated();
+
+// Computer c1 = new Computer(1, "2gb", "i3");
+// context.Computers.Add(c1);
+// context.SaveChanges();
+
+// Computer c2 = new Computer(2, "2gb", "i3");
+// context.Computers.Add(c2);
+
+// Computer c3 = new Computer(3, "2gb", "i3");
+// context.Computers.Add(c3);
+// context.SaveChanges();
+
+IEnumerable<Computer> computers = context.Computers;
+
+foreach (var item in computers)
+{
+    Console.WriteLine($"{item.Id}, {item.Ram}, {item.Processor}");
+}
+
+Computer encontrado = context.Computers.Find(2);
+Console.WriteLine($"{encontrado.Id}, {encontrado.Ram}, {encontrado.Processor}");
+
+encontrado.Ram = "20GB";
+encontrado.Processor = "amd";
+context.Computers.Update(encontrado);
+context.SaveChanges();
+
+Console.WriteLine(context.Computers.Find(2));
+
+context.Computers.Remove(encontrado);
+context.SaveChanges();
+
+computers = context.Computers;
+
+foreach (var item in computers)
+{
+    Console.WriteLine($"{item.Id}, {item.Ram}, {item.Processor}");
+}
